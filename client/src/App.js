375eb311c05b848d48d0285fb6dbcbf131d0ddf1
Kayte <kaytemcdonough@gmail.com>
2021-06-15 20:32:05 -0400

    editing app.js with current logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo from "./BarU-logo.png";
 // import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+        <div className="App-header">
+        <img src={logo}  alt="logo" />
+        <h2>Welcome to BarU</h2>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
     </div>
- );
+  );
 }
-
 
 export default App;
