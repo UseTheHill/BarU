ad455ea0073484272291ee5059667524a2fcd49f
Dracon75 <nanorth75@gmail.com>
2021-06-15 22:50:23 -0400

    routing all the way

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        BarU
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/login"
+              className={window.location.pathname === "/login" ? "nav-link active" : "nav-link"}
+            >
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/post"
+              className={window.location.pathname === "/post" ? "nav-link active" : "nav-link"}
+            >
+              Post
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/profile"
+              className={window.location.pathname === "/profile" ? "nav-link active" : "nav-link"}
+            >
+              Profile
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/search"
+              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+            >
+              Search
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ client/src/pages/ProfilePage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../../BarU-logo.png";
+
+function ProfilePageScreen () {
+    return(
+    <div className="App">
+        <div className="App-header">
+          <img src={logo}  alt="logo" />
+          <h2>Welcome to the BarU ProfilePage!</h2>
+        </div>
+    </div>
+    )
+}
+
+export default ProfilePageScreen;
