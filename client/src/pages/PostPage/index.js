5f20872e97d80249726fb4f9c7c42f4c988af6c6
ellisa <ellisaissa@gmail.com>
2021-06-17 21:09:08 -0400

    changes post and profile pages resolved merge commits

--- client/src/pages/PostPage/index.js
+++ client/src/pages/PostPage/index.js
@@ -1,15 +1,62 @@
-import React from "react";
-import logo from "../../BarU-logo.png";
 
-function PostPageScreen () {
-    return(
-    <div className="App">
-        <div className="App-header">
-          <img src={logo}  alt="logo" />
-          <h2>Welcome to the BarU PostPage!</h2>
-        </div>
-    </div>
-    )
-}
+// import React from "react";
+// import logo from "../../BarU-logo.png";
 
-export default PostPageScreen;
+// function PostPageScreen () {
+//     return(
+//     <div className="App">
+//         <div className="App-header">
+//           <img src={logo}  alt="logo" />
+//           <h2>Welcome to the BarU PostPage!</h2>
+//         </div>
+//     </div>
+//     )
+// }
+
+// export default PostPageScreen;
+
+// import React from "react";
+
+import React, { Component } from "react";
+import Nav from "../../components/Nav";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+class PostPageScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drinkname: '',
+      Barname: null,
+    };
+  }
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
+  render() {
+    return (
+      <div>
+        <form>
+        <h1>{this.state.drinkname} {this.state.Barname}</h1>
+        <p>Enter the drink name:</p>
+        <input
+          type='text'
+          name='drink name'
+          onChange={this.myChangeHandler}
+        />
+        <p>Enter bar name:</p>
+        <input
+          type='text'
+          Barname='bar name'
+          onChange={this.myChangeHandler}
+        />
+        <p>Enter the description of the drink:</p>
+          <textarea type="type"></textarea>
+        </form>
+      </div>
+    );
+    };}
+
+    export default PostPageScreen;
+
--- client/src/pages/ProfilePage/index.js
+++ client/src/pages/ProfilePage/index.js
@@ -11,5 +11,4 @@ function ProfilePageScreen () {
     </div>
     )
 }
-
 export default ProfilePageScreen;
