a269d212445be196b6a29800948f42dd09fd8686
Kayte <kaytemcdonough@gmail.com>
2021-06-24 20:45:42 -0400

    added attach photo option to post page

--- client/src/pages/PostPage/index.js
+++ client/src/pages/PostPage/index.js
@@ -75,9 +75,39 @@ class PostPageScreen extends Component {
               type="type"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             ></textarea>
+            <div class="grid grid-cols-1 space-y-2">
+              <label class="text-sm font-bold text-gray-500 tracking-wide">
+                Attach Photo of Your Cocktail
+              </label>
+              <div class="flex items-center justify-center w-full">
+                <label class="flex flex-col rounded-lg border-4 border-dashed w-full h-20 p-10 group text-center">
+                  <div class="h-full w-full text-center flex flex-col justify-center items-center  ">
+                    <div class="flex flex-auto max-h-20 w-2/5 mx-auto -mt-10"></div>
+                    <p class="pointer-none text-gray-500">
+                      <a href="" id="" class="text-blue-600 hover:underline">
+                        Select a file.
+                      </a>
+                    </p>
+                  </div>
+                  {/* <input type="file" class="hidden"> */}
+                </label>
+              </div>
+            </div>
+
+            <p class="text-sm text-gray-300">
+              <span>File type: types of images</span>
+            </p>
+            <div>
+              <button
+                type="submit"
+                class="flex justify-center bg-blue-200 hover:bg-blue-500 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Upload
+              </button>
+            </div>
             <div className="flex justify-center">
               <button
-                className="bg-pink-200 hover:bg-pink-500 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                className="bg-pink-200 hover:bg-pink-500 text-gray-800 font-bold my-4 py-2 px-8 rounded focus:outline-none focus:shadow-outline tracking-wide w-full"
                 type="button"
               >
                 <Link to="/profile">Submit</Link>
