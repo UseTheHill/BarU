ff0927f2911154e1449163f71921fa6c7b86434e
Kayte <kaytemcdonough@gmail.com>
2021-07-01 19:42:59 -0400

    updated post page

--- client/src/pages/PostPage/index.js
+++ client/src/pages/PostPage/index.js
@@ -102,40 +102,7 @@ class PostPageScreen extends Component {
               onChange={this.myChangeHandler}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             ></textarea>
-            <div className="grid grid-cols-1 space-y-2">
-              <label className="text-sm font-bold text-gray-500 tracking-wide">
-                Attach Photo of Your Cocktail
-              </label>
-              <div className="flex items-center justify-center w-full">
-                <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-20 p-10 group text-center">
-                  <div className="h-full w-full text-center flex flex-col justify-center items-center  ">
-                    <div className="flex flex-auto max-h-20 w-2/5 mx-auto -mt-10"></div>
-
-                    <input type="file" onChange={this.imageChangeHandler} />
-                    <img src={this.state.file} />
-
-                    <p className="pointer-none text-gray-500">
-                      <a href="" id="" className="text-blue-600 hover:underline">
-                        Select a file.
-                      </a>
-                    </p>
-                  </div>
-                  {/* <input type="file" className="hidden"> */}
-                </label>
-              </div>
-            </div>
-
-            <p className="text-sm text-gray-300">
-              <span>File type: types of images</span>
-            </p>
-            <div>
-              <button
-                type="submit"
-                className="flex justify-center bg-blue-200 hover:bg-blue-500 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              >
-                Upload
-              </button>
-            </div>
+            
             <div className="flex justify-center">
               <button /*onChange={this.myChangeHandler}*/
                 className="bg-pink-200 hover:bg-pink-500 text-gray-800 font-bold my-4 py-2 px-8 rounded focus:outline-none focus:shadow-outline tracking-wide w-full"
